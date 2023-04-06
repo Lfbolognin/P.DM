c7c071681ff0caa3756e2eabdb934aab5e245727
Lfbolognin <126827723+Lfbolognin@users.noreply.github.com>
2023-04-06 14:31:57 -0400

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+    home: PaginaInicial()
+    
+    );
+  }
+}
+
+class PaginaInicial extends StatelessWidget {
+  const PaginaInicial({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlueAccent,
+        title: Text("Olá Meu App "),
+      ),
+      body: Container(),
+    );
+  }
+}
